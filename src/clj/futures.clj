d21fb78ddf00e8e79dd4a27ee55964e6a75c26ab
unknown <timothypratley@gmail.com>
2009-02-02 10:35:55 +1100

    updated futures to be cancelable

--- src/clj/futures.clj
+++ src/clj/futures.clj
@@ -12,12 +12,26 @@
     ;(.submit clojure.lang.Agent/soloExecutor func))
   (defn fwait [#^java.util.concurrent.Future fut]
     (.get fut))
+  (defn fcancel [#^java.util.concurrent.Future fut]
+    (.cancel fut true))
   (defn shutdown-futures []
     (.shutdown pool)
     (.shutdown solo)))
 
 (println "solo" (fwait (send-off-future #(+ 1 2))))
 (println "pool" (fwait (send-future #(+ 1 2))))
+(defn long-calc []
+  (dotimes [i 1000]
+    (reduce * (range 2 1000))))
+
+(println "Start long-calc")
+(time (fcancel (send-future long-calc)))
+(println "Cancelled")
+(println "Start long-calc")
+(time (fwait (send-future long-calc)))
+(println "Done")
+
+
 (shutdown-futures)
 
 

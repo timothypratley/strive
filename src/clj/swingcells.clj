a5e080f81ae2355ed2bce9346fa3cb0c12ec4f86
unknown <Ninja@.(none)>
2009-03-06 02:05:50 +1100

    More functionality

--- src/clj/swingcells.clj
+++ src/clj/swingcells.clj
@@ -1,18 +1,22 @@
-
+; Copyright (c) Timothy Pratley. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+; which can be found in the file epl-v10.html at the root of this distribution.
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+; You must not remove this notice, or any other, from this software.
 
 (set! *warn-on-reflection* true)
-(ns timothypratley.swingcells
+(ns swingcells
   (:import
      (clojure.lang Agent Atom Ref)
-     (java.awt Component Window GridBagLayout GridBagConstraints
+     (java.awt Component GridBagLayout GridBagConstraints
                GraphicsEnvironment)
      (java.awt.event ActionListener)
-     (javax.swing JTextField JLabel JPanel JPasswordField JButton JFrame
-                  SwingUtilities UIManager ImageIcon)
-     (javax.swing.event DocumentListener)
+     (javax.swing JTextField JLabel JPanel JPasswordField JFrame
+                  ImageIcon)
      (javax.sound.midi MidiSystem)
      (javax.sound.sampled AudioSystem DataLine$Info Clip AudioInputStream)
-     (java.util.concurrent LinkedBlockingQueue)
      (java.io File)))
 
 
@@ -40,21 +44,21 @@
 (defmacro later
   "Invokes body on the swing event thread"
   [& body]
-  `(SwingUtilities/invokeLater (fn [] ~@body)))
+  `(javax.swing.SwingUtilities/invokeLater (fn [] ~@body)))
 
 (defmacro add-action-listener
   "Attaches an ActionListener to a Component"
-  [#^Component obj, evt & body]
+  [#^java.awt.Component obj, evt & body]
   `(.addActionListener
-     ~obj (proxy [ActionListener] []
+     ~obj (proxy [java.awt.event.ActionListener] []
             (actionPerformed [~evt] ~@body))))
 
 (defmacro add-text-listener
   "Attaches a TextListener to a Component"
-  [#^Component obj, evt & body]
+  [#^java.awt.Component obj, evt & body]
   `(.addDocumentListener
      (.getDocument ~obj)
-     (proxy [DocumentListener] []
+     (proxy [javax.swing.event.DocumentListener] []
        (changedUpdate [~evt] ~@body)
        (insertUpdate [~evt] ~@body)
        (removeUpdate [~evt] ~@body))))
@@ -62,17 +66,17 @@
 (defmacro button
   "Create a JButton with an ActionListener"
   [#^String text, evt & body]
-  `(doto (JButton. ~text) (add-action-listener ~evt ~@body)))
+  `(doto (javax.swing.JButton. ~text) (add-action-listener ~evt ~@body)))
 
 (defmacro frame
   "Construct a frame"
   [#^String title & body]
-  `(let [f# (JFrame. ~title)]
+  `(let [f# (javax.swing.JFrame. ~title)]
      (later 
-       (UIManager/setLookAndFeel
-         (UIManager/getSystemLookAndFeelClassName))
+       (javax.swing.UIManager/setLookAndFeel
+         (javax.swing.UIManager/getSystemLookAndFeelClassName))
        (doto f#
-         (.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
+         (.setDefaultCloseOperation (javax.swing.JFrame/EXIT_ON_CLOSE))
          (.setLayout (GridBagLayout.))
          ~@body
          (.pack)
@@ -89,7 +93,7 @@
 
 (defn full-screen
   "Enables full-screen mode"
-  [#^Window window]
+  [#^java.awt.Window window]
   (later
     (->
       (GraphicsEnvironment/getLocalGraphicsEnvironment)
@@ -101,8 +105,8 @@
   blocker is a symbol for use in creating a navigation event.
   Put the next transition onto blocker to move to a new screen.
   Transitions match value taken from blocker, last odd is default."
-  [#^Window window, blocker elements transitions]
-  `(let [~blocker (LinkedBlockingQueue.)]
+  [#^java.awt.Window window, blocker elements transitions]
+  `(let [~blocker (java.util.concurrent.LinkedBlockingQueue.)]
      (later (doseq [e# ~elements]
               (apply add ~window (first e#) (rest e#)))
             (.validate ~window)
@@ -114,14 +118,14 @@
 
 (defn navigate
   "Moves through predefined screens."
-  [#^Window window, next]
+  [#^java.awt.Window window, next]
   (if-let [f (first next)]
     (recur window (f window (second next)))
     (.dispose window)))
 
 (defn set-background
   "Sets an image as the background of a JFrame"
-  [#^Window window, #^String filename]
+  [#^java.awt.Window window, #^String filename]
   (let [image (.getImage (ImageIcon. filename))]
     (doto window
       (.setContentPane
@@ -260,7 +264,7 @@
       (.close))))
 
 
-; Examples
+; Example
 
 (defn simple-demo []
   ; Define the data model
@@ -275,7 +279,6 @@
       (Thread/sleep 2000)
       (swap! temp #(+ %1 -1 (rand 2.5)))
       ; Meanwhile the textbox can also update the temperature.
-      ; You have to be quick type a new number and press enter.
       (recur))))
 
 ;(defn dynamic-demo []
@@ -283,100 +286,4 @@
          ;(button "Add entry" (add-entry))
          ;(button "View table" (view-table))))
 
-
-; Game demo
-
-(defn login
-  "Pseudo login check, returns user details"
-  [#^String username, #^String password]
-  (if true 
-    ;(and (zero? (.compareToIgnoreCase username "Conan"))
-    ;     (= password "Barbarian"))
-    {:username username,
-     :characters [{:name "Maldy", :class :warrior}
-                  {:name "Than", :class :warlock}
-                  {:name "Alsvid" :class :hunter}]}
-    false))
-
-(def login-screen)
-(defn play-screen
-  [#^Window window, character]
-  (stop-midi)
-  (set-background window "play.png")
-  (play-midi "HUMAN1.MID")
-  (screen window blocker
-          [[(button "Bye" evt
-                    (stop-midi)
-                    (set-background window "splash.jpg")
-                    (play-midi "TITLE.MID")
-                    (.put blocker true))]]
-          [true login-screen]))
-
-(defn character-create-screen
-  [#^Window window, _]
-  (let [character-name (atom "")]
-    (screen window blocker
-            [[(text-field character-name "Enter your chacter's name:")]
-             [(button "Confirm" evt (if (pos? (count @character-name))
-                                      (.put blocker @character-name)))
-              :gridx 1]
-             [(button "Cancel" evt (.put blocker false))
-              :gridx 2]]
-            [false login-screen play-screen])))
-
-(let [icons {:warrior (ImageIcon. "warrior.jpg")
-             :hunter (ImageIcon. "hunter.jpg")
-             :warlock (ImageIcon. "warlock.jpg")}]
-  (defn character-select-screen
-    [#^Window window, user]
-    (screen window blocker
-            (conj 
-              (vec (map #(list
-; oh my I'm out of whitespace
-(doto (button (str (:name %1) " - " (name (:class %1)))
-              evt (.put blocker (:name %1)))
-  (.setIcon (icons (:class %1))))
-                           :gridx 1)
-                        (user :characters)))
-              [(button "Create New" evt (.put blocker :create-new))
-               :gridx 1])
-            [:create-new character-create-screen
-             false login-screen play-screen])))
-
-(defn login-screen
-  [#^Window window, _]
-  (let [username (atom "")
-        password (atom "")]
-    (screen window blocker
-            [[(text-field username "Username:")
-              :gridx 1, :gridy 1]
-             [(password-field password "Password:")
-              :gridx 1, :gridy 2]
-             [(JLabel. "Hint: Conan/Barbarian")
-              :gridx 2, :gridy 2]
-             [(button "Login" evt (.put blocker (login @username @password)))
-              :gridx 1, :gridy 4]
-             [(button "Quit" evt (.put blocker :quit))
-              :gridx 2, :gridy 5]]
-            [:quit nil false login-screen character-select-screen])))
-
-(defn splash-screen
-  [#^Window window, _]
-  (set-background window "splash.jpg")
-  (play-midi "TITLE.MID")
-  (screen window blocker
-          [[(button "FunMud" evt (.put blocker true))]]
-          [login-screen]))
-
-
-(defn game-demo []
-  (navigate
-    (doto
-      (frame "FunMud")
-      (full-screen)
-      (.setUndecorated true))
-    [splash-screen nil])
-  (stop-midi))
-
-(game-demo)
 

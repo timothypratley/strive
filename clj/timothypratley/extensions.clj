85a60a60f259c5a7c23538abfa0d37cfa843ed88
tpratley <timothypratley@gmail.com>
2009-07-07 16:01:10 +1000

    Initial implementation of messenger with tests

---
+++ clj/timothypratley/extensions.clj
@@ -0,0 +1,112 @@
+(ns timothypratley.extensions
+  (:use [clojure.contrib.str-utils :only (re-sub)])
+  (:import (java.util.logging Logger Level Formatter ConsoleHandler)
+     (java.text SimpleDateFormat)
+     (java.util Date)))
+
+(defmacro while-let
+  "while with a binding"
+  [[v cnd] & body]
+  `(loop[]
+     (let [~v ~cnd]
+       (when ~v
+         ~@body
+         (recur)))))
+(defmacro while-let-pred
+  "while with a binding and predicate"
+  [[v cnd] pred & body]
+  `(loop[]
+     (let [~v ~cnd]
+       (when (~pred ~v)
+         ~@body
+         (recur)))))
+
+; Stephen C. Gilardi   	
+(defn unmangle
+  "Given the name of a class that implements a Clojure function,  
+  returns the function's name in Clojure.
+  Note: If the true Clojure function name contains any underscores
+  (a rare occurrence), the unmangled name will contain hyphens
+  at those locations instead."
+  [class-name]
+  (.replace
+    (re-sub #"^(.+)\$(.+)__\d+$" "$1/$2" class-name)
+    \_ \-))
+
+; Stephen C. Gilardi   	
+(defmacro current-function-name
+  "Returns a string, the name of the current Clojure function"
+  []
+  `(-> (Throwable.) .getStackTrace first .getClassName unmangle))
+
+(defmacro defn-doc
+  "TODO: fix me"
+  [fun doc-str & body]
+  `(defn ~(str doc-str) ~fun ~@body))
+
+(defmacro str-now
+  [s]
+  `~(str s))
+
+(let [logger (Logger/getLogger "timothypratley.extensions.log")
+      a (agent nil)
+      m {:off Level/OFF
+         :severe Level/SEVERE
+         :warning Level/WARNING
+         :info Level/INFO
+         :config Level/CONFIG
+         :fine Level/FINE
+         :finer Level/FINER
+         :finest Level/FINEST}]
+  (.addHandler logger (ConsoleHandler.))
+  (.setUseParentHandlers logger false)
+
+  (defn log*
+    "Log a message."
+    [level c f & text]
+    (send-off a (fn [_]
+                  (.logp logger (m level) c f (apply str text)))))
+
+  (defmacro log
+    "Log a message.
+    Buffers messages sequentially so multiple thread logs will not overlap.
+    level should be one of:
+    (keys m)"
+    [level & text]
+    `(log* ~level
+           (.getName (Thread/currentThread))
+           (current-function-name)
+           ~@text))
+
+  (defn log-level
+    "Sets the current log level to one of:
+    (keys m)"
+    [level]
+    (.setLevel logger (m level))
+    (doseq [h (.getHandlers logger)]
+      (.setLevel h (m level))))
+
+  (defn log-format
+    "Sets the log formatter"
+    ([]
+     (log-format
+          (let [sdf (SimpleDateFormat. "yyyy/MM/dd HH:mm:ss")]
+            (proxy [Formatter] []
+              (format [r]
+                      (str "[" (.format sdf (Date. (.getMillis r)))
+                           " " (.getLevel r)
+                           "]: " (.getMessage r)
+                           " [" (.getSourceClassName r)
+                           " " (.getSourceMethodName r)
+                           "]" \newline))))))
+    ([formatter] 
+     (doseq [h (.getHandlers logger)]
+       (.setFormatter h formatter)))))
+
+(defmacro debug
+  "Debugging shorthand.
+  Will evaluate and log debug info about expr.
+  NB: you must (log-level :fine) to see the output."
+  [expr]
+  `(let [a# ~expr] (log :fine "DEBUG:" '~expr "=" a#) a#))
+
---
+++ clj/timothypratley/test-messenger.clj
@@ -0,0 +1,29 @@
+(ns test-messenger
+  (:use clojure.test timothypratley.messenger timothypratley.extensions))
+
+(log-format)
+(log-level :finest)
+
+(deftest sends-and-receives-messages
+  (let [message {:id 404 :data "hello world"}
+        result (ref nil)
+        server-protocol (fn [m]
+                          (log :info "Server received: " m)
+                          ; NB: this goes outside the test scope
+                          ; (is (= m message))
+                          (dosync (ref-set result m))
+                          :bye)
+        client-protocol (fn [m]
+                          (log :info "Client recieved: " m)
+                          :bye)
+        listener (listen 8888 server-protocol)
+        conn (connect "localhost" 8888 client-protocol)]
+    (send-message conn message)
+    (.close listener)
+    ; allow the threads some time to work
+    (Thread/sleep 1000)
+    (is (= @result message))))
+
+(run-tests)
+(shutdown-agents)
+

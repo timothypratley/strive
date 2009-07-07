27c48d396cb483771ce8ea0e08f07de5f2ee606a
tpratley <timothypratley@gmail.com>
2009-07-07 21:13:18 +1000

    updated logging functions

--- clj/timothypratley/logging.clj
+++ clj/timothypratley/logging.clj
@@ -2,7 +2,9 @@
   (:use [clojure.contrib.str-utils :only (re-sub)])
   (:import (java.util.logging Logger Level Formatter ConsoleHandler)
      (java.text SimpleDateFormat)
-     (java.util Date)))
+     (java.util Date)
+     (java.io ByteArrayOutputStream PrintWriter PrintStream
+              OutputStreamWriter OutputStream)))
 
 ; Stephen C. Gilardi   	
 (defn unmangle
@@ -27,10 +29,6 @@
   [fun doc-str & body]
   `(defn ~(str doc-str) ~fun ~@body))
 
-(defmacro str-now
-  [s]
-  `~(str s))
-
 (let [logger (Logger/getLogger "timothypratley.extensions.log")
       a (agent nil)
       m {:off Level/OFF
@@ -46,9 +44,9 @@
 
   (defn log*
     "Log a message."
-    [level c f & text]
+    [level thread fun & text]
     (send-off a (fn [_]
-                  (.logp logger (m level) c f (apply str text)))))
+                  (.logp logger (m level) thread fun (apply str text)))))
 
   (defmacro log
     "Log a message.
@@ -84,12 +82,71 @@
                            "]" \newline))))))
     ([formatter] 
      (doseq [h (.getHandlers logger)]
-       (.setFormatter h formatter)))))
+       (.setFormatter h formatter))))
+  
+  (defn log-wait
+    "Waits for logging to finish."
+    []
+    (await a)))
 
 (defmacro debug
   "Debugging shorthand.
   Will evaluate and log debug info about expr.
   NB: you must (log-level :fine) to see the output."
   [expr]
-  `(let [a# ~expr] (log :fine "DEBUG:" '~expr "=" a#) a#))
+  `(let [a# ~expr] (log :fine "DEBUG " '~expr "=" a#) a#))
+
+; http://blogs.sun.com/nickstephen/entry/java_redirecting_system_out_and
+(defn logging-output-stream
+  "Creates a logging output stream useful for capturing stdout and stderr."
+  [level]
+  (PrintStream.
+    (proxy [ByteArrayOutputStream] []
+      (flush []
+        (proxy-super flush)
+        (let [s (.trim (.toString this))]
+          (proxy-super reset)
+          (if (> (.length s) 0)
+            (log level s)))))
+    true))
+
+(comment
+(defn logging-output-stream
+  [level]
+  (PrintStream.
+      (proxy [ByteArrayOutputStream] []
+        (write [b]
+            (proxy-super write b)))))
+
+(defn logging-output-stream
+  [level]
+  (PrintStream.
+    (let [baos (ByteArrayOutputStream.)]
+      (proxy [OutputStream] []
+        (write [b]
+          (if (= b \newline)
+            (do (log level (.trim (.toString baos)))
+              (.reset baos))
+            (.write baos b)))))))
+  )
+
+(defmacro log-capture
+  "Captures stderr and stdout for logging.
+  Multiple thread access can still mix up output,
+  and the thread/function will not be correct.
+  Prefer to still use log instead of print,
+  this is just to catch unexpected output."
+  [& body]
+  `(let [new-out# (logging-output-stream :info)
+         new-err# (logging-output-stream :warning)
+         old-out# (System/out)
+         old-err# (System/err)]
+     (System/setOut new-out#)
+     (System/setErr new-err#)
+     (binding [*out* (PrintWriter. new-out# true)
+               *err* (PrintWriter. new-err# true)]
+       ~@body
+       (log-wait))
+     (System/setOut old-out#)
+     (System/setErr old-err#)))
 
